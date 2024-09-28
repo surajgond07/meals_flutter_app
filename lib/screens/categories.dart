d5e94e7cf7a7814f15bf7ea57da6fff712b8c09b
Suraj Gond <surajgond07@gmail.com>
2024-09-28 08:51:36 +0530

    Adding Meal Data

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+// import 'package:meals_app/models/category.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -9,19 +12,17 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your Category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisExtent: 20,
+          mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('1'),
-          Text('2'),
-          Text('3'),
-          Text('4'),
-          Text('5'),
-          Text('6'),
+        children: [
+          // availableCategories.map((category => CategoryGridItem(category: category).toList() ))
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
